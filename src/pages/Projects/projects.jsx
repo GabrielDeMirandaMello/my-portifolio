7a11189c4da3c771975a3dc55e533cd77f1cade4
Gabriel Mello <gabriel.mmello@sptech.school>
2024-03-27 17:02:20 -0300

    ajustes

--- src/pages/Projects/projects.jsx
+++ src/pages/Projects/projects.jsx
@@ -13,24 +13,24 @@ function Projects() {
                     <h1 className='title-aboutme'>{`<Meus Projetos />`}</h1>
                     <Card 
                     title={'History Travels (Front-End)'}
-                    description={'Aonde pude aprender sobre varias coisas relacionadas a criação, manutenção entre outras formas de lidar com empreendimento de uma empresa.'}
-                    period={'Periodo: Janeiro de 2015 à Dezembro de 2019'}
+                    description={'Pequena descrição das tecnologias usadas.'}
+                    period={'Periodo: Janeiro de 2022 à Dezembro de 2023'}
                     github={'true'}
                     link={'https://github.com/GabrielDeMirandaMello/NewIndividualProjectFrontEnd'}
                     />
                     <Card 
                     title={'History Travels (Back-End)'}
-                    description={'Aonde pude aprender sobre varias coisas relacionadas a criação, manutenção entre outras formas de lidar com empreendimento de uma empresa.'}
-                    period={'Periodo: Janeiro de 2015 à Dezembro de 2019'}
+                    description={'Pequena descrição das tecnologias usadas.'}
+                    period={'Periodo: Janeiro de 2022 à Dezembro de 2023'}
                     github={'true'}
                     link={'https://github.com/GabrielDeMirandaMello/NewIndividualProjectBackEnd'}
                     />
                     <Card 
                     title={'Cross Game (Faculdade)'}
-                    description={'Aonde pude aprender sobre Logica de programação, Modelo Agil(Scrum), como trabalhar em uma equipe em pró do projeto, Programação Orientada a Objeto, Desenvolvimento de Apps com Kotlin e saber mais sobre como funciona a nuvem e o Hardware de um computador.'}
+                    description={'Pequena descrição das tecnologias usadas.'}
                     period={'Periodo: Janeiro de 2022 à Dezembro de 2023'}
                     github={'true'}
-                    link={'none'}
+                    link={'https://github.com/orgs/Cross-Game/repositories'}
                     />
                 </div>
             </section>
