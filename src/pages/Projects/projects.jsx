6dd51dabdf671f2ee09f4805ae6e7d6d4c653914
Gabriel Mello <gabriel.mmello@sptech.school>
2024-03-31 12:54:10 -0300

    atualizando projetos

--- src/pages/Projects/projects.jsx
+++ src/pages/Projects/projects.jsx
@@ -13,15 +13,15 @@ function Projects() {
                     <h1 className='title-aboutme'>{`<Meus Projetos />`}</h1>
                     <Card 
                     title={'History Travels (Front-End)'}
-                    description={'Pequena descrição das tecnologias usadas.'}
-                    period={'Periodo: Janeiro de 2022 à Dezembro de 2023'}
+                    description={'Este projeto foi feito com o intuito de aprimorar meus conhecimentos, consiste em uma rede social para pessoas que gostam de viajar e poder compartilhar locais com as pessoas e somente compartilhar as viajens. Tecnologias e Ferramentas usadas no Front End: JavaScript, React.js, HTML 5, CSS3 e Node.js.'}
+                    period={'Periodo: Janeiro de 2024 à Março de 2024'}
                     github={'true'}
                     link={'https://github.com/GabrielDeMirandaMello/NewIndividualProjectFrontEnd'}
                     />
                     <Card 
                     title={'History Travels (Back-End)'}
-                    description={'Pequena descrição das tecnologias usadas.'}
-                    period={'Periodo: Janeiro de 2022 à Dezembro de 2023'}
+                    description={'Este projeto foi feito com o intuito de aprimorar meus conhecimentos, consiste em uma rede social para pessoas que gostam de viajar e poder compartilhar locais com as pessoas e somente compartilhar as viajens. Tecnologias e Ferramentas usadas no Back End: Java, Spring Boot, PostgreSQL, Docker, Maven, JUnit, Clean Architecture, JiraSoftware, Render.'}
+                    period={'Periodo: Periodo: Janeiro de 2024 à Março de 2024'}
                     github={'true'}
                     link={'https://github.com/GabrielDeMirandaMello/NewIndividualProjectBackEnd'}
                     />
