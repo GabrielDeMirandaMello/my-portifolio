6d21385c6aee1ee6fe5fc37bf64bcc67bb815d0f
Gabriel Mello <gabriel.mmello@sptech.school>
2024-06-19 21:38:42 -0300

    wip

--- src/pages/About_Me/aboutme.jsx
+++ src/pages/About_Me/aboutme.jsx
@@ -20,25 +20,25 @@ function Aboutme() {
                 <div className='container-conteudo-aboutme'>
                     <h1 className='title-aboutme-contato'>{`<Mais Sobre Min/>`}</h1>
                     <div className='title-formacao-aboutme'>
-                        <h1 className='title-formacao-card'><BsFillMotherboardFill style={{width:'70px', height:'auto'}}/> {`<Tecnologias e Ferramentas />`}</h1>
+                        <h1 className='title-formacao-card'><BsFillMotherboardFill className='icon-aboutme'/> {`<Tecnologias e Ferramentas />`}</h1>
                         <h3>Aqui você verá mais sobre as Ferramentas e Tecnologias que já usei !</h3>
                         <button onClick={() => navigate('/tecnologias')}>Ir para Tecnologias e Ferramentas</button>
                     </div>
 
                     <div className='title-formacao-aboutme'>
-                        <h1 className='title-formacao-card'><FaMedal  style={{width:'70px', height:'auto'}}/> {`<Formação />`}</h1>
+                        <h1 className='title-formacao-card'><FaMedal  className='icon-aboutme'/> {`<Formação />`}</h1>
                         <h3>Aqui você verá mais sobre meus diplomas e cursos !</h3>
                         <button onClick={() => navigate('/formacao')}>Ir para Formação</button>
                     </div>
                     
                     <div className='title-formacao-aboutme'>
-                        <h1 className='title-formacao-card'><FaUserTie  style={{width:'70px', height:'auto'}}/> {`<Experiencias />`}</h1>
+                        <h1 className='title-formacao-card'><FaUserTie  className='icon-aboutme'/> {`<Experiencias />`}</h1>
                         <h3>Aqui irei falar sobre as experiencias profissionais que tive até o momento !</h3>
                         <button onClick={() => navigate('/experiencia')}>Ir para Experiencias</button>
                     </div>
                     
                     <div className='title-formacao-aboutme'>
-                        <h1 className='title-formacao-card'><FaIcons style={{width:'70px', height:'auto'}}/> {`<Interesses />`}</h1>
+                        <h1 className='title-formacao-card'><FaIcons className='icon-aboutme'/> {`<Interesses />`}</h1>
                         <h3>Aqui irei mostrar mais sobre os meus hobbys e o que faço no tempo livre ! </h3>
                         <button onClick={() => navigate('/interesse')}>Ir para interesses</button>
                     </div>
