b78a107c04f944dcc5e2755882b7c8e3d6500c42
Gabriel Mello <gabriel.mmello@sptech.school>
2024-06-30 16:26:18 -0300

    wip

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -7,18 +7,17 @@ function Home() {
     return (
         <>
             <Navigation item={'pagina inicial'} />
-            <section className='container-home mb-10 object-cover cursor-default'>
+            <section className='container-home mb-10 object-cover'>
                 <div className='container-conteudo h-fit flex justify-center items-center'>
-                    <h1 className='text-5xl pb-3 mx-5 text-blue-500 font-black'>Gabriel De Miranda Mello</h1>
-                    <div className='texto-home mx-5 text-base'>
-                        <p> Olá! Sou um profissional apaixonado por Tecnologia. Estou aqui para compartilhar um pouco sobre mim e minhas habilidades.
-                            Desde jovem, sempre fui fascinado por Tecnologia. Ao longo dos anos, dediquei-me a aprimorar minhas habilidades e conhecimentos nesse campo, buscando constantemente novos desafios e oportunidades de crescimento.
-                            <br />
-                            Estou animado para compartilhar mais sobre meu trabalho e experiências. Convido você a explorar meu portfólio, onde você encontrará uma amostra dos projetos que realizei, as habilidades que possuo. Se você estiver interessado em colaborar ou tiver alguma pergunta, não hesite em entrar em contato. Estou ansioso para conectar-me.
+                    <h1 className='text-5xl pb-3 mx-5 my-5 text-blue-500 font-black'>Gabriel De Miranda Mello</h1>
+                    <div className='texto-home mx-5 text-lg text-justify w-3/4'>
+                        <p> Olá! Sou <span className='text-blue-500 font-black'>Desenvolvedor Back-End</span> apaixonado por Tecnologia. Estou aqui para compartilhar um pouco sobre mim e as tecnologias que utilizo.
+                            Ao longo dos anos, dediquei-me a aprimorar minhas habilidades e conhecimentos nesse campo, buscando constantemente novos desafios e oportunidades de crescimento.
+                            Convido você a explorar meu portfólio, onde você encontrará uma amostra dos projetos que realizei e das habilidades que possuo.
                         </p>
                     </div>
-                    <hr className='mt-10 mb-10' />
-                    <h1 className='text-3xl pb-3 mx-5 text-blue-500 font-bold'> Habilidades Tecnicas e Ferramentas</h1>
+                    <hr className='w-3/4 mt-5 mb-5' />
+                    <h1 className='text-3xl pb-3 mx-5 text-blue-500 font-black'> Habilidades Tecnicas e Ferramentas</h1>
 
                     <div className='flex flex-wrap w-auto'>
                         <div className='flex flex-wrap w-auto'>
@@ -51,7 +50,7 @@ function Home() {
                                     </p>
                                     <div class="flex justify-end space-x-4">
                                         <button
-                                            class="duration-300 bg-black/0 hover:bg-black/25 text-white font-bold py-2 px-4 rounded"
+                                            class="duration-300 bg-black/0 hover:bg-black/25 text-white font-bold py-2 px-4 rounded cursor-default"
                                         >
                                             Nivel Intermediario
                                         </button>
@@ -70,7 +69,7 @@ function Home() {
                                     </p>
                                     <div class="flex justify-end space-x-4">
                                         <button
-                                            class="duration-300 bg-black/0 hover:bg-black/25 text-white font-bold py-2 px-4 rounded"
+                                            class="duration-300 bg-black/0 hover:bg-black/25 text-white font-bold py-2 px-4 rounded cursor-default"
                                         >
                                             Nivel Avançado
                                         </button>
@@ -79,8 +78,8 @@ function Home() {
                             </div>
                         </div>
                     </div>
-                    <hr className='mt-10 mb-10' />
-                    <h1 className='text-3xl pb-3 mx-5 text-blue-500 font-bold'>Formação e Cursos</h1>
+                    <hr className='w-3/4  mt-5 mb-5' />
+                    <h1 className='text-3xl pb-3 mx-5 text-blue-500 font-black'>Formação e Cursos</h1>
                     <div className='flex flex-wrap w-auto'>
                         <div className='flex flex-wrap w-auto'>
                             <div
@@ -93,7 +92,7 @@ function Home() {
                                     </p>
                                     <div class="flex justify-end space-x-4">
                                         <button
-                                            class="duration-300 bg-black/0 hover:bg-black/25 text-white font-bold py-2 px-4 rounded"
+                                            class="duration-300 bg-black/0 hover:bg-black/25 text-white font-bold py-2 px-4 rounded cursor-default"
                                         >
                                             Bacharel Concluido
                                         </button>
@@ -112,7 +111,7 @@ function Home() {
                                     </p>
                                     <div class="flex justify-end space-x-4">
                                         <button
-                                            class="duration-300 bg-black/0 hover:bg-black/25 text-white font-bold py-2 px-4 rounded"
+                                            class="duration-300 bg-black/0 hover:bg-black/25 text-white font-bold py-2 px-4 rounded cursor-default"
                                         >
                                             Bacharel Concluido
                                         </button>
@@ -124,6 +123,9 @@ function Home() {
                     </div>
                 </div>
             </section>
+            <footer className='w-full flex justify-center'> 
+                <h4 className='font-black'>Criado por Gabriel Mello, 2024</h4>
+            </footer>
 
         </>
     )
