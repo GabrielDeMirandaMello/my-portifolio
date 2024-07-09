a6492f830b230414a11c0ef5e5aa34a81e526c62
Gabriel Mello <gabriel.mmello@sptech.school>
2024-07-09 16:47:50 -0300

    wip home

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -13,7 +13,7 @@ function Home() {
         },
         {
             title: "JavaScript",
-            description: "",
+            description: "Variáveis e Tipos de Dados, Funções, Objetos e Protótipos, Escopo, Closure, Eventos, Assincronia e Manipulação do DOM",
             levelOfExperience: "Nivel Intermediario"
         }
     ]
@@ -62,61 +62,41 @@ function Home() {
     ]
     const tools = [
         {
-            title: "GitFlow",
-            description: "",
-            levelOfExperience: "Nivel Avançado"
-        },
-        {
             title: "Docker",
-            description: "Conceitos Básicos(Contêineres, Imagens), Comandos Básicos( docker run, docker pull, docker build, docker ps, docker rm e docker rmi), Dockerfile e Docker Compose.",
-            levelOfExperience: "Nivel Intermediario"
-        },
-        {
-            title: "Kubernetes",
-            description: "",
+            description: "Docker é uma plataforma popular para desenvolver, enviar e executar aplicativos usando containerização. Conceitos Básicos(Contêineres, Imagens), Comandos Básicos( docker run, docker pull, docker build, docker ps, docker rm e docker rmi), Dockerfile e Docker Compose.",
             levelOfExperience: "Nivel Intermediario"
         },
         {
             title: "AWS",
-            description: "",
-            levelOfExperience: "Nivel Intermediario"
+            description: "AWS, ou Amazon Web Services, é uma plataforma de computação em nuvem abrangente e amplamente usada oferecida pela Amazon. Ela fornece uma ampla gama de serviços em nuvem, incluindo poder de computação, armazenamento, bancos de dados, aprendizado de máquina, análise, rede e muito mais. Criação de maquinas EC2 e atraves desta maquina conseguir subir uma aplicação completa.",
+            levelOfExperience: "Nivel Basico"
         },
         {
-            title: "Kafka",
-            description: "",
-            levelOfExperience: "Nivel Intermediario"
-        },
-        {
-            title: "Postman",
-            description: "",
-            levelOfExperience: "Nivel Intermediario"
-        },
-        {
-            title: "Maven",
-            description: "",
-            levelOfExperience: "Nivel Intermediario"
-        },
-        {
-            title: "Kibana",
-            description: "",
+            title: "Apache Kafka",
+            description: "Apache Kafka é uma plataforma de software de processamento de fluxo de código aberto desenvolvida pela Apache Software Foundation, escrita em Scala e Java. Ela visa fornecer uma plataforma unificada, de alto rendimento e baixa latência para lidar com feeds de dados em tempo real.",
             levelOfExperience: "Nivel Intermediario"
         },
         {
             title: "Minio",
-            description: "",
+            description: "O MinIO é um sistema de armazenamento de objetos de alto desempenho e código aberto. Ele é compatível com o serviço de armazenamento em nuvem Amazon S3 e foi projetado para aplicativos nativos da nuvem, oferecendo recursos como escalabilidade, multilocação e resiliência.",
             levelOfExperience: "Nivel Intermediario"
         },
         {
             title: "DBeaver",
-            description: "",
+            description: "O DBeaver é uma ferramenta de banco de dados universal gratuita e de código aberto para desenvolvedores, programadores SQL, administradores de banco de dados e analistas. Ele suporta vários bancos de dados, incluindo MySQL, PostgreSQL, SQLite, Oracle, DB2, SQL Server, Sybase, MS Access, Teradata, Firebird, Apache Hive, Phoenix e muitos mais.",
             levelOfExperience: "Nivel Intermediario"
+        },
+        {
+            title: "GitFlow",
+            description: "Tags, Master, Hotfix, Release Branch, develop e Feature Branch.",
+            levelOfExperience: "Nivel Avançado"
         }
     ]
     const others = [
         {
             title: "Jira",
             description: "",
-            levelOfExperience: "Nivel Avançado"
+            levelOfExperience: "Nivel Intermediario"
         },
         {
             title: "Bitbucket",
@@ -148,78 +128,83 @@ function Home() {
                             Convido você a explorar meu portfólio, onde você encontrará uma amostra dos projetos que realizei e das habilidades que possuo.
                         </p>
                     </div>
-                    <hr className='w-3/4 mt-5 mb-5' />
+                    <hr className='w-11/12 mt-5 mb-5' />
                     <h1 className='text-3xl pb-3 mx-5 text-blue-500 font-black'> Linguagens que aprendi em minha carreira como Dev</h1>
 
-                    <div className='flex flex-wrap w-auto'>
+                    <div className='flex flex-wrap w-3/4'>
                         {languages.map((language) => (
                             <div className='flex flex-wrap w-auto'>
-                            <div
-                                className="bg-gradient-to-r m-5 from-blue-400 to-indigo-500 rounded-lg overflow-hidden shadow-xl max-w-xs"
-                            >
-                                <div className="p-4">
-                                    <h2 className="text-lg font-semibold mb-2 text-white">{language.title}</h2>
-                                    <p className="text-sm mb-4 text-white">
-                                        {language.description}
-                                    </p>
-                                    <div className="flex justify-end space-x-4">
-                                        <button
-                                            className="duration-300 bg-black/0 hover:bg-black/25 text-white font-bold py-2 px-4 rounded cursor-default"
-                                        >
-                                            {language.levelOfExperience}
-                                        </button>
+                                <div
+                                    className="bg-gradient-to-r m-5 from-blue-400 to-indigo-500 rounded-lg overflow-hidden shadow-xl max-w-xs"
+                                >
+                                    <div className="p-4">
+                                        <h2 className="text-lg font-semibold mb-2 text-white">{language.title}</h2>
+                                        <p className="text-sm mb-4 text-white">
+                                            {language.description}
+                                        </p>
+                                        <div className="flex justify-end space-x-4">
+                                            <button
+                                                className="duration-300 bg-black/0 hover:bg-black/25 text-white font-bold py-2 px-4 rounded cursor-default"
+                                            >
+                                                {language.levelOfExperience}
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
                         ))}
-                        
+
 
                     </div>
-                    <hr className='w-3/4  mt-5 mb-5' />
+                    <hr className='w-11/12  mt-5 mb-5' />
                     <h1 className='text-3xl pb-3 mx-5 text-blue-500 font-black'>Tecnologias de Back End Aprendidas</h1>
+                    <div className='flex flex-wrap w-11/12'>
+                        {tools.map((language) => (
+                            <div className='flex flex-wrap w-auto'>
+                                <div
+                                    className="bg-gradient-to-r m-5 from-blue-400 to-indigo-500 rounded-lg overflow-hidden shadow-xl max-w-xs"
+                                >
+                                    <div className="p-4">
+                                        <h2 className="text-lg font-semibold mb-2 text-white">{language.title}</h2>
+                                        <p className="text-sm mb-4 text-white">
+                                            {language.description}
+                                        </p>
+                                        <div className="flex justify-end space-x-4">
+                                            <button
+                                                className="duration-300 bg-black/0 hover:bg-black/25 text-white font-bold py-2 px-4 rounded cursor-default"
+                                            >
+                                                {language.levelOfExperience}
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <hr className='w-11/12  mt-5 mb-5' />
+                    <h1 className='text-3xl pb-3 mx-5 text-blue-500 font-black'>Outras Ferramentas</h1>
                     <div className='flex flex-wrap w-auto'>
-                        <div className='flex flex-wrap w-auto'>
-                            <div
-                                className="bg-gradient-to-r m-5 from-blue-400 to-indigo-500 rounded-lg overflow-hidden shadow-xl max-w-xs"
-                            >
-                                <div className="p-4">
-                                    <h2 className="text-lg font-semibold mb-2 text-white">Administração</h2>
-                                    <p className="text-sm mb-4 text-white">
-                                        Planejamento, Organização, Direção, Controle, Gestão de Pessoas (Recursos Humanos), Gestão Financeira, Marketing, Gestão da Produção e Operações, Tecnologia da Informação (TI), Ética e Responsabilidade Social.
-                                    </p>
-                                    <div className="flex justify-end space-x-4">
-                                        <button
-                                            className="duration-300 bg-black/0 hover:bg-black/25 text-white font-bold py-2 px-4 rounded cursor-default"
-                                        >
-                                            Bacharel Concluido
-                                        </button>
+                        {others.map((language) => (
+                            <div className='flex flex-wrap w-auto'>
+                                <div
+                                    className="bg-gradient-to-r m-5 from-blue-400 to-indigo-500 rounded-lg overflow-hidden shadow-xl max-w-xs"
+                                >
+                                    <div className="p-4">
+                                        <h2 className="text-lg font-semibold mb-2 text-white">{language.title}</h2>
+                                        <p className="text-sm mb-4 text-white">
+                                            {language.description}
+                                        </p>
+                                        <div className="flex justify-end space-x-4">
+                                            <button
+                                                className="duration-300 bg-black/0 hover:bg-black/25 text-white font-bold py-2 px-4 rounded cursor-default"
+                                            >
+                                                {language.levelOfExperience}
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className='flex flex-wrap w-auto'>
-                            <div
-                                className="bg-gradient-to-r m-5 from-blue-400 to-indigo-500 rounded-lg overflow-hidden shadow-xl max-w-md"
-                            >
-                                <div className="p-4">
-                                    <h2 className="text-lg font-semibold mb-2 text-white">Análise e Desenvolvimento de Sistemas</h2>
-                                    <p className="text-sm mb-4 text-white">
-                                        Habilidades adquiridas e usadas: 1° Semestre: Javascript, node.js, HTML e CSS, Mysql e Git.
-                                        2° Semestre: Java, Javascript, node.js, HTML, CSS, Mysql, Git.
-                                        3° Semestre React.js e CSS para o front-end, Spring Boot, Spring Security, JWT, Mysql.
-                                        4° Semestre Realizando a criação do APP android do projeto do 3° semestre usando kotlin.
-                                    </p>
-                                    <div className="flex justify-end space-x-4">
-                                        <button
-                                            className="duration-300 bg-black/0 hover:bg-black/25 text-white font-bold py-2 px-4 rounded cursor-default"
-                                        >
-                                            Bacharel Concluido
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </section>
