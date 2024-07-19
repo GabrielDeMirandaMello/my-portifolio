b071d42d87c626d04199c541fd3ca67fd8fe2fb3
Gabriel Mello <gabriel.mmello@sptech.school>
2024-07-18 21:48:09 -0300

    wip

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -143,7 +143,24 @@ function Home() {
                                     <h1 className='text-3xl pb-3 mx-5 text-blue-500 font-black'> Linguagens que aprendi em minha carreira como Dev</h1>
 
                                     <div className='flex flex-wrap w-3/4'>
-                                        {languages.map((language) => (
+                                        <div class="bg-transparent py-10 sm:py-10">
+                                            <div class="mx-10 px-6 lg:px-8">
+                                                <div class="mx-10 mt-10 grid grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+                                                    <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/transistor-logo-white.svg" alt="Transistor" width="158" height="48" />
+                                                    <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/reform-logo-white.svg" alt="Reform" width="158" height="48" />
+                                                    <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/tuple-logo-white.svg" alt="Tuple" width="158" height="48" />
+                                                    <img class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/savvycal-logo-white.svg" alt="SavvyCal" width="158" height="48" />
+                                                    <img class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/statamic-logo-white.svg" alt="Statamic" width="158" height="48" />
+                                                    <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/transistor-logo-white.svg" alt="Transistor" width="158" height="48" />
+                                                    <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/reform-logo-white.svg" alt="Reform" width="158" height="48" />
+                                                    <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/tuple-logo-white.svg" alt="Tuple" width="158" height="48" />
+                                                    <img class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/savvycal-logo-white.svg" alt="SavvyCal" width="158" height="48" />
+                                                    <img class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/statamic-logo-white.svg" alt="Statamic" width="158" height="48" />
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        {/* {languages.map((language) => (
                                             <div className='flex flex-wrap w-auto'>
                                                 <div
                                                     className="bg-gradient-to-r m-5 from-blue-400 to-indigo-500 rounded-lg overflow-hidden shadow-xl max-w-xs"
@@ -163,7 +180,7 @@ function Home() {
                                                     </div>
                                                 </div>
                                             </div>
-                                        ))}
+                                        ))} */}
 
 
                                     </div>
