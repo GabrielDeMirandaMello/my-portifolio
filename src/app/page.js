90d10307b9596bdceb9faeef44d18bdd57199dd3
GabrielDeMirandaMello <Gabriel.mmello@sptech.school>
2025-02-17 17:39:36 -0300

    #mobile navigation and menu finished

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,119 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
+import { HiMenu, HiOutlineX } from "react-icons/hi";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <div className="w-screen h-fit flex flex-col">
+        <header className="fixed top-0 left-0 w-screen h-fit p-5 flex justify-around bg-black border-b-blue-400 border-b-2 md:border-none">
+          <nav className="flex flex-col justify-between md:flex-row md:justify-around">
+            <div className="mx-5 text-blue-400 font-semibold">
+              Gabriel de Miranda
+            </div>
+
+            <div className="hidden md:flex md:gap-5 lg:gap-10 xl:gap-20 lg:w-[350px] md:w-[200px]">
+              <a
+                href="#Bebidas"
+                className="text-white hover:text-blue-400 lg:text-2xl relative group"
+              >
+                Bebidas
+                <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
+              </a>
+              <a
+                href="#Relatos"
+                className="text-white hover:text-blue-400 lg:text-2xl relative group"
+              >
+                Relatos
+                <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
+              </a>
+              <a
+                href="#Contato"
+                className="text-white hover:text-blue-400 lg:text-2xl relative group"
+              >
+                Contato
+                <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
+              </a>
+            </div>
+            {isOpen && (
+            <div className="md:hidden bg-black shadow-md space-y-5 mt-5 ">
+              <a
+                onClick={() => setIsOpen(false)}
+                href="#Inicio"
+                className="block px-4 py-2 text-white hover:text-blue-400 relative group"
+              >
+                Inicio
+                <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
+              </a>
+              <a
+                onClick={() => setIsOpen(false)}
+                href="#Sobremim"
+                className="block px-4 py-2 text-white hover:text-blue-400 relative group"
+              >
+                Sobre mim
+                <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
+              </a>
+              <a
+                onClick={() => setIsOpen(false)}
+                href="#Projetos"
+                className="block px-4 py-2 text-white hover:text-blue-400 relative group"
+              >
+                Projetos
+                <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
+              </a>
+              <a
+                onClick={() => setIsOpen(false)}
+                href="#Conhecimentos"
+                className="block px-4 py-2 text-white hover:text-blue-400 relative group"
+              >
+                Conhecimentos
+                <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
+              </a>
+              <a
+                onClick={() => setIsOpen(false)}
+                href="#Contato"
+                className="block px-4 py-2 text-white hover:text-blue-400 relative group"
+              >
+                Contato
+                <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
+              </a>
+            </div>
+          )}
+          </nav>
+          
+          <div className="md:hidden lg:hidden xl:hidden">
+            <button onClick={() => setIsOpen(!isOpen)}>
+              {isOpen ? <HiOutlineX size={24} color="white" style={{ marginRight: '20px' }} /> : <HiMenu size={24} color="white" style={{ marginRight: '20px' }} />}
+            </button>
+          </div>
+
+        </header>
+        <main className="w-screen h-screen">
+          <section id="Inicio" className="w-screen h-screen bg-black">
+            Inicio
+          </section>
+          <section id="SobreMim" className="w-screen h-screen">
+            Sobre mim
+          </section>
+          <section id="Projetos" className="w-screen h-screen">
+            Projetos
+          </section>
+          <section id="Conhecimentos" className="w-screen h-screen">
+            Conhecimentos
+          </section>
+          <section id="Contato" className="w-screen h-screen">
+            Contato
+          </section>
+          <footer className="w-screen h-20 justify-center items-center">
+            Created By Gabriel De Miranda
+          </footer>
+
+          
+        </main>
+      </div>
+    </>
   );
 }
